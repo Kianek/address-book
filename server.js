ad6e43aa123a4ba74bd7b80aeef9592af12bff6e
Keanu Allridge <kallridge92@gmail.com>
2018-11-08 07:00:36 -0800

    Configure server

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 const helmet = require('helmet');
 const bodyParser = require('body-parser');
+const contacts = require('./routes/api/contacts');
+const users = require('./routes/api/users');
 
 const app = express();
 
@@ -18,6 +20,22 @@ app.use(
     saveUninitialized: false,
   })
 );
+app.use(passport.initialize());
 app.use(passport.session());
 
-//
+// Initialize MongoDB
+const db = require('./config/mongo').uri;
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log(err));
+
+// Load Routes
+// app.use('/api/users', users);
+// app.use('/api/contacts', contacts);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
