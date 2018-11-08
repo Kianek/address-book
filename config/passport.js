6bbf7d7510e792ca8e64ffa6fa86cf516d3d1381
Keanu Allridge <kallridge92@gmail.com>
2018-11-08 06:51:19 -0800

    Configure local strategy for passport

--- config/passport.js
+++ config/passport.js
@@ -2,4 +2,37 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
-module.exports = passport => {};
+const User = require('../models/User');
+
+module.exports = passport => {
+  passport.use(
+    new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
+      User.findOne({ email }).then(user => {
+        if (!user) {
+          return done(null, false);
+        }
+
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+
+          if (isMatch) {
+            return done(null, user);
+          }
+          return done(null, false);
+        });
+      });
+    })
+  );
+
+  passport.serializeUser((user, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+
+  passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+      done(err, user);
+    });
+  });
+};
