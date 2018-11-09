4d24961407d939170b4000633f23c0fcf35bca5b
Keanu Allridge <kallridge92@gmail.com>
2018-11-08 19:56:30 -0800

    Add route HTTP verb boilerplate

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,4 +5,24 @@ router.get('/test', (req, res) => {
   res.json({ msg: 'Contacts works' });
 });
 
+// GET /api/contacts/
+// Get all contacts
+router.get('/', (req, res) => {});
+
+// POST /api/contacts/add
+// Add a new contact
+router.post('/add', (req, res) => {});
+
+// PUT /api/contacts/:id/update
+// Update a single contact
+router.put('/:id/update', (req, res) => {});
+
+// DELETE /api/contacts/:id/delete
+// Delete a single contact
+router.delete('/:id/delete', (req, res) => {});
+
+// DELETE /api/contacts/delete-all
+// Delete all contacts
+router.delete('/delete-all', (req, res) => {});
+
 module.exports = router;
