f1448aeecc8d76c1f43faaa5d5d61058a51f4adb
Keanu Allridge <kallridge92@gmail.com>
2018-12-03 07:15:01 -0800

    Update requires in preparation for deployment

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Initialize MongoDB
-const db = require('./config/mongo').uri;
+const db = require('./config/keys_dev').uri;
 mongoose
   .connect(
     db,
