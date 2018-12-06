ab9743722a8d26a45bd6c9b7c63a0b191d5e580c
Keanu Allridge <kallridge92@gmail.com>
2018-12-06 14:01:53 -0800

    Create InputField component

---
+++ client/src/components/common/input-field/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './InputField.scss';
+
+function InputField({ type, label, name, placeholder, value, onChange }) {
+  return (
+    <div class="input-group">
+      {label && (
+        <React.Fragment>
+          <label className="input-field__label">{label}:</label>
+          <br />
+        </React.Fragment>
+      )}
+      <input
+        className="input-field"
+        type={type}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+}
+
+InputField.defaultProps = {
+  type: 'text',
+};
+
+InputField.propTypes = {
+  type: PropTypes.string.isRequired,
+  label: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.any,
+  onChange: PropTypes.func,
+};
+
+export default InputField;
