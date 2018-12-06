986c95aa935d6f355157ef11c705414864cf188c
Keanu Allridge <kallridge92@gmail.com>
2018-12-06 14:02:05 -0800

    Create Contacts component

---
+++ client/src/components/contacts/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Contacts extends Component {
+  render() {
+    return (
+      <div>
+        Header and buttons
+        <ul>
+          <li>Contact</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Contacts;
