77f72e5dae82de5977ef628883b1a8fec784c5f7
Keanu Allridge <kallridge92@gmail.com>
2018-12-06 13:56:14 -0800

    Add Router and Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.scss';
+import Navbar from './components/navbar/Navbar';
+import Login from './components/login';
+import Register from './components/register';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Stuff</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/register" component={Register} />
+        </div>
+      </Router>
     );
   }
 }
