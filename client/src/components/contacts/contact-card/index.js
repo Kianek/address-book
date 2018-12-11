adfd1cb04b43e54e90c1d7c5f1e99adf64793584
Keanu Allridge <kallridge92@gmail.com>
2018-12-11 06:24:35 -0800

    Add body and styles to component

--- client/src/components/contacts/contact-card/index.js
+++ client/src/components/contacts/contact-card/index.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import './ContactCard.scss';
+
 function ContactCard(props) {
-  return <div>Stuff</div>;
+  return (
+    <li className="contact-card">
+      <h2 className="heading">First Middle Last</h2>
+      <p>Phone: 555-555-5555</p>
+      <p>Email: cortez@williams.com</p>
+      <h3 className="heading">Address</h3>
+      <div className="street-address">
+        <p>Line 1</p>
+        <p>Line 2</p>
+      </div>
+      <div className="city-state-zip">
+        <p>City:</p>
+        <p>State:</p>
+        <p>Zip:</p>
+      </div>
+    </li>
+  );
 }
 
 export default ContactCard;
