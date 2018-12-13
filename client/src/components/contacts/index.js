6ce5cd2d777aea1d8512ad0ca5873c4c7890f78c
Keanu Allridge <kallridge92@gmail.com>
2018-12-13 14:02:59 -0800

    Fill in lifecycle method, render method, etc.

--- client/src/components/contacts/index.js
+++ client/src/components/contacts/index.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {
+  isContactsEmpty,
+  selectAllContacts,
+  isLoading,
+} from '../../redux/reducers/user';
+import { fetchContacts } from '../../redux/reducers/user/actions';
+import { Link } from 'react-router-dom';
+import ContactCard from './contact-card';
+
+import './Contacts.scss';
+import Loading from '../common/spinner';
 
 class Contacts extends Component {
+  componentDidMount() {
+    this.props.fetchContacts();
+  }
+
+  format = contacts => contacts.map(contact => <ContactCard {...contact} />);
+
   render() {
+    const { contacts, isContactsEmpty, isLoading } = this.props;
+
     return (
-      <div>
-        Header and buttons
-        <ul>
-          <li>Contact</li>
+      <div className="contacts">
+        <h1 className="contacts__heading">Contacts</h1>
+        <div className="contacts__controls">
+          <Link to="/add" className="contacts__controls__button--add">
+            <i className="fas fa-plus" /> Add
+          </Link>
+          <button className="contacts__controls__button--delete-all">
+            <i className="fas fa-times" /> Delete All
+          </button>
+        </div>
+        <ul className="contacts__list">
+          {isLoading ? (
+            <Loading />
+          ) : isContactsEmpty ? (
+            <div className="contacts__list__message">No contacts yet</div>
+          ) : (
+            this.format(contacts)
+          )}
         </ul>
       </div>
     );
   }
 }
 
-export default Contacts;
+const mapStateToProps = state => ({
+  contacts: selectAllContacts(state),
+  isContactsEmpty: isContactsEmpty(state),
+  isLoading: isLoading(state),
+});
+
+export default connect(
+  mapStateToProps,
+  { fetchContacts }
+)(Contacts);
