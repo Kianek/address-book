6ebc0d23c531518d89aeda6545f9cf619af8e3df
Keanu Allridge <kallridge92@gmail.com>
2018-12-13 16:31:22 -0800

    Add routes, private routes, and switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PrivateRoute from './components/common/PrivateRoute';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Login from './components/login';
 import Register from './components/register';
+import Contacts from './components/contacts';
+import AddContact from './components/add';
+import EditContact from './components/edit';
 
 class App extends Component {
   render() {
@@ -12,7 +16,12 @@ class App extends Component {
         <div className="App">
           <Navbar />
           <Route exact path="/" component={Login} />
-          <Route exact path="/register" component={Register} />
+          <Route path="/register" component={Register} />
+          <Switch>
+            <PrivateRoute path="/contacts" component={Contacts} />
+            <PrivateRoute path="/add" component={AddContact} />
+            <PrivateRoute path="/:id/edit" component={EditContact} />
+          </Switch>
         </div>
       </Router>
     );
