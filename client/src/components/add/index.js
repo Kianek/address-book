9cf547a55e8321059f79e146a54d5f16880eb2e4
Keanu Allridge <kallridge92@gmail.com>
2018-12-19 12:02:14 -0800

    Remove unused code

--- client/src/components/add/index.js
+++ client/src/components/add/index.js
@@ -9,7 +9,6 @@ import {
 } from '../../redux/reducers/user/actions';
 import { Link } from 'react-router-dom';
 import { isEmpty, createNewContactFrom } from '../../helpers';
-import '../../App.scss';
 import Form from '../common/form';
 import InputField from '../common/input-field';
 
@@ -50,79 +49,77 @@ class AddContact extends Component {
     const { error } = this.props;
 
     return (
-      <div className="add-contact-page">
-        <div className="form-container">
-          <Link to="/contacts" className="form__button--back">
-            <i className="fas fa-arrow-circle-left" />
-            Back to Contacts
-          </Link>
-          <Form title="Add Contact" onSubmit={this.onSubmit}>
-            <InputField
-              label="First Name"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Middle Name"
-              name="middleName"
-              placeholder="(Optional)"
-              value={this.state.middleName}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Last Name"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Phone"
-              name="phone"
-              value={this.state.phone}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Email"
-              name="email"
-              value={this.state.email}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Line 1"
-              name="line1"
-              value={this.state.line1}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Line 2"
-              name="line2"
-              placeholder="(Optional)"
-              value={this.state.line2}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="City"
-              name="city"
-              value={this.state.city}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="State"
-              name="state"
-              value={this.state.state}
-              onChange={this.onChange}
-            />
-            <InputField
-              label="Zip"
-              name="zip"
-              value={this.state.zip}
-              onChange={this.onChange}
-            />
-            {error && <div className="form__error">{error.msg}</div>}
-            <button className="form__submit">Submit</button>
-          </Form>
-        </div>
+      <div className="form-container">
+        <Link to="/contacts" className="form__button--back">
+          <i className="fas fa-arrow-circle-left" />
+          Back to Contacts
+        </Link>
+        <Form title="Add Contact" onSubmit={this.onSubmit}>
+          <InputField
+            label="First Name"
+            name="firstName"
+            value={this.state.firstName}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Middle Name"
+            name="middleName"
+            placeholder="(Optional)"
+            value={this.state.middleName}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Last Name"
+            name="lastName"
+            value={this.state.lastName}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Phone"
+            name="phone"
+            value={this.state.phone}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Email"
+            name="email"
+            value={this.state.email}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Line 1"
+            name="line1"
+            value={this.state.line1}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Line 2"
+            name="line2"
+            placeholder="(Optional)"
+            value={this.state.line2}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="City"
+            name="city"
+            value={this.state.city}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="State"
+            name="state"
+            value={this.state.state}
+            onChange={this.onChange}
+          />
+          <InputField
+            label="Zip"
+            name="zip"
+            value={this.state.zip}
+            onChange={this.onChange}
+          />
+          {error && <div className="form__error">{error.msg}</div>}
+          <button className="form__submit">Submit</button>
+        </Form>
       </div>
     );
   }
